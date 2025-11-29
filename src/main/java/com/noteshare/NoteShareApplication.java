3b7825112c1e93c1141ee217dd3220b16e13dd58
fengshiping <3165374007@qq.com>
2025-11-29 11:16:21 +0800

    第一天后端任务量，注册API开发，登录API与Session管理

---
+++ src/main/java/com/noteshare/NoteShareApplication.java
@@ -0,0 +1,11 @@
+package com.noteshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteShareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoteShareApplication.class, args);
+    }
+}
